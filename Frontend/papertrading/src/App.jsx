2ded4284511265771fdae6d24f1ee04ab775b97e
Pratham083 <prathamp083@gmail.com>
2025-04-17 16:40:13 -0400

    project setup

--- Frontend/papertrading/src/App.jsx
+++ Frontend/papertrading/src/App.jsx
@@ -1,17 +1,10 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ padding: '2rem' }}>
+      <h1>Hello React + CSS Modules</h1>
+    </div>
   );
 }
 
